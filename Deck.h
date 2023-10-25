5c0351785775e8020186eb37753c25066cb24625
Steven P Jones <106358275+SPJII@users.noreply.github.com>
2023-10-25 13:07:04 -0400

    Update Deck.h
    attributes declared

--- Deck.h
+++ Deck.h
@@ -1,6 +1,19 @@
-#pragma once
-class Deck
-{
-	//P1 code here
+#ifndef DECK_H
+#define DECK_H
+
+#include <list>
+#include <vector>
+
+class Deck {
+public:
+    Deck();
+    int drawCard();
+    void returnCards(const std::vector<int>& wonCards);
+    int cardsLeft() const;
+    void addCard(int card);
+
+private:
+    std::list<int> cards;
 };
 
+#endif
