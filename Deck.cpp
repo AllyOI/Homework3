de7e0ec35ba485602ca8389d9547d7a1ae8a8bec
Steven P Jones <106358275+SPJII@users.noreply.github.com>
2023-10-25 13:12:36 -0400

    Update Deck.cpp

--- Deck.cpp
+++ Deck.cpp
@@ -1,1 +1,30 @@
 #include "Deck.h"
+
+Deck::Deck() {
+    // Initialize a standard deck of cards (52 cards)
+    for (int suit = 0; suit < 4; ++suit) {
+        for (int rank = 2; rank <= 14; ++rank) {
+            cards.push_back(rank);
+        }
+    }
+}
+
+int Deck::drawCard() {
+    int card = cards.front();
+    cards.pop_front();
+    return card;
+}
+
+void Deck::returnCards(const std::vector<int>& wonCards) {
+    for (int card : wonCards) {
+        cards.push_back(card);
+    }
+}
+
+int Deck::cardsLeft() const {
+    return cards.size();
+}
+
+void Deck::addCard(int card) {
+    cards.push_back(card);
+}
